ca6acbfc258a30469672fcf4320f156a5a6908f7
Himanshu111001 <shishodia1977@gmail.com>
2023-01-26 15:28:08 +0530

    bootstrap form and cards for notes added

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,89 @@
+import NoteContext from "./noteContext";
+import { useState } from "react";
+
+const NoteState = (props) => {
+  const notesInitial = [
+    {
+      _id: "63d01e24ebb54c24d4f5522f",
+      user: "63cfecef2df1dba1e3ef7b26",
+      title: "Mera Title",
+      description: "eat momos daily else regret",
+      tag: "personal",
+      date: "2023-01-24T18:06:28.177Z",
+      __v: 0,
+    },
+    {
+      _id: "63d01f206df360133d73c7df",
+      user: "63cfecef2df1dba1e3ef7b26",
+      title: "Iphone khareedo",
+      description: "2023 m kuch bi krke iphone lena h",
+      tag: "personal",
+      date: "2023-01-24T18:10:40.697Z",
+      __v: 0,
+    },
+    {
+      _id: "63d01e24ebb54c24d4f5522f",
+      user: "63cfecef2df1dba1e3ef7b26",
+      title: "Mera Title",
+      description: "eat momos daily else regret",
+      tag: "personal",
+      date: "2023-01-24T18:06:28.177Z",
+      __v: 0,
+    },
+    {
+      _id: "63d01f206df360133d73c7df",
+      user: "63cfecef2df1dba1e3ef7b26",
+      title: "Iphone khareedo",
+      description: "2023 m kuch bi krke iphone lena h",
+      tag: "personal",
+      date: "2023-01-24T18:10:40.697Z",
+      __v: 0,
+    },
+    {
+      _id: "63d01e24ebb54c24d4f5522f",
+      user: "63cfecef2df1dba1e3ef7b26",
+      title: "Mera Title",
+      description: "eat momos daily else regret",
+      tag: "personal",
+      date: "2023-01-24T18:06:28.177Z",
+      __v: 0,
+    },
+    {
+      _id: "63d01f206df360133d73c7df",
+      user: "63cfecef2df1dba1e3ef7b26",
+      title: "Iphone khareedo",
+      description: "2023 m kuch bi krke iphone lena h",
+      tag: "personal",
+      date: "2023-01-24T18:10:40.697Z",
+      __v: 0,
+    },
+    {
+      _id: "63d01e24ebb54c24d4f5522f",
+      user: "63cfecef2df1dba1e3ef7b26",
+      title: "Mera Title",
+      description: "eat momos daily else regret",
+      tag: "personal",
+      date: "2023-01-24T18:06:28.177Z",
+      __v: 0,
+    },
+    {
+      _id: "63d01f206df360133d73c7df",
+      user: "63cfecef2df1dba1e3ef7b26",
+      title: "Iphone khareedo",
+      description: "2023 m kuch bi krke iphone lena h",
+      tag: "personal",
+      date: "2023-01-24T18:10:40.697Z",
+      __v: 0,
+    },
+  ];
+
+  const [notes, setNotes] = useState(notesInitial);
+
+  return (
+    <NoteContext.Provider value={{ notes, setNotes }}>
+      {props.children}
+    </NoteContext.Provider>
+  );
+};
+
+export default NoteState;
